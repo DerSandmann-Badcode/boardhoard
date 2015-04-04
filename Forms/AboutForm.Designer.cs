3d6d90b65571012f30f72fd069cc4af463208e79
Max <maximilian.schwartz@gmail.com>
2015-04-04 23:53:25 +0200

    Updated Version Number

--- Forms/AboutForm.Designer.cs
+++ Forms/AboutForm.Designer.cs
@@ -84,7 +84,7 @@
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(192, 25);
             this.label1.TabIndex = 7;
-            this.label1.Text = "BoardHoard v0.3.0";
+            this.label1.Text = "BoardHoard v0.3.1";
             // 
             // btnAboutOK
             // 
---
+++ Stats.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoardHoard
+{
+    public class Stats
+    {
+        public int SiteCount;
+        public int ThreadCount;
+        public int StaticImageCount;
+        public int AnimatedImageCount;
+        public int HTMLDownloadCount;
+    }
+}
